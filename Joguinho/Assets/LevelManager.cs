6c6effd5714ebf40d8f70f25e80629de25e28f4f
Luis Vogt <luishenriquevogt@gmail.com>
2016-08-20 22:11:32 -0300

    Tiles com transição linear e acelerada adicionados

---
+++ Joguinho/Assets/LevelManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	Transform tempChild;
+	Vector3 tempPointFinish;
+	float tempSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < transform.childCount; i++)
+		{
+			tempChild = transform.GetChild(i);
+			if(tempChild.GetComponent<TileScript>().getTransitionState()!=TileScript.transitionState.DONE)
+			{
+				tempPointFinish = tempChild.GetComponent<TileScript>().getPointFinish();
+				tempSpeed = tempChild.GetComponent<TileScript>().getTransitionSpeed();
+			}
+			if(tempChild.GetComponent<TileScript>().getTransitionState() == TileScript.transitionState.UP)
+			{
+				if(tempChild.transform.position.y <= tempPointFinish.y)
+				{
+					tempChild.transform.position = tempPointFinish;
+					tempChild.GetComponent<TileScript>().finishTransition();
+				}
+				else
+				{
+					tempChild.transform.position = Vector3.MoveTowards(tempChild.transform.position,tempPointFinish,tempSpeed);	
+					tempChild.GetComponent<TileScript>().addTransitionTime(Time.deltaTime);
+				}
+			}
+		}
+	}
+}
---
+++ Joguinho/Assets/TileScript.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class TileScript: MonoBehaviour {
+
+	//O enum transitionState indica de onde o bloco aparece na transição.
+	public enum transitionState {DONE,UP,DOWN};
+	public enum transitionSpeedType {LINEAR,ACCELERATION};
+	//pointStart e pointFinish indicam onde o bloco começa e termina na transição. De modo a simplificar a edição de 
+	//cenários, pointFinish é o ponto onde o bloco está no editor.
+	Vector3 pointStart;
+	Vector3 pointFinish;
+
+	float transitionTime = 0;
+
+	public float transitionSpeed = 1.0f;
+	public float transitionAcceleration = 0.01f;
+	public transitionState transition = transitionState.UP;
+	public transitionSpeedType speedType = transitionSpeedType.ACCELERATION;
+	public transitionState getTransitionState()
+		{
+			return transition;
+		}
+
+	public Vector3 getPointFinish()
+	{
+		return pointFinish;
+	}
+
+	public void finishTransition()
+	{
+		transition=transitionState.DONE;
+	}
+
+	public float getTransitionSpeed()
+	{
+		if(speedType==transitionSpeedType.LINEAR)
+			{
+				return transitionSpeed;		
+			}
+		else if (speedType==transitionSpeedType.ACCELERATION)
+		{
+			transitionSpeed = transitionSpeed + transitionTime * transitionAcceleration;
+			return transitionSpeed;
+		}
+		else 
+		{
+			return 0;
+		}
+	}
+
+	public void addTransitionTime(float t)
+	{
+		transitionTime += t;
+	}
+
+
+	void Start()
+	{
+		if (getTransitionState() != transitionState.DONE)
+		{
+			pointFinish = transform.position;
+			if(getTransitionState() == transitionState.UP)
+			{
+				pointStart = pointFinish;
+				pointStart.y = pointStart.y + 5;
+			}
+
+			transform.position = pointStart;
+		}
+	}
+
+}
