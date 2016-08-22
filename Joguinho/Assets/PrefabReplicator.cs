db9c0fc28e1f22b5f5a2120a9e9581d3eabda2cc
Luis Vogt <luishenriquevogt@gmail.com>
2016-08-21 22:00:19 -0300

    Editor customizado adicionado para facilitar a criação em massa de prefabs

---
+++ Joguinho/Assets/PrefabReplicator.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrefabReplicator : MonoBehaviour {
+
+	public bool onePoint;
+	public Vector3 initPoint;
+	public Vector3 finalPoint;
+	public float dist = 1.0f;
+
+	private Vector3 point;
+
+	public GameObject prefabby;
+
+	private GameObject temp;
+
+	public void CopyPrefab()
+	{
+		if(onePoint)
+		{
+			Instantiate(prefabby, initPoint,Quaternion.identity,this.transform);
+		}
+		else
+		{
+			point.x = (int)(finalPoint.x - initPoint.x);
+			point.y = (int)(finalPoint.y - initPoint.y);
+			point.z = (int)(finalPoint.z - initPoint.z);
+			for(int x = 0; x < Mathf.Abs(point.x)+1; x++)
+			{
+				for(int y = 0; y < Mathf.Abs(point.y)+1; y++)
+				{
+					for(int z = 0; z < Mathf.Abs(point.z)+1; z++)
+					{
+						temp = Instantiate(prefabby, new Vector3(x*dist*Mathf.Sign(point.x),y*dist*Mathf.Sign(point.y),z*dist*Mathf.Sign(point.z)), 
+							Quaternion.identity, this.transform) as GameObject;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Joguinho/Assets/Scripts/Editor/EditorCreatePrefabs.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(PrefabReplicator))]
+public class EditorCreatePrefabs : Editor {
+	public override void OnInspectorGUI()
+	{
+		PrefabReplicator myPrefabReplicator = (PrefabReplicator)target;
+		myPrefabReplicator.onePoint = EditorGUILayout.Toggle(myPrefabReplicator.onePoint);
+		myPrefabReplicator.initPoint = EditorGUILayout.Vector3Field("Ponto Inicial",myPrefabReplicator.initPoint);
+		if(myPrefabReplicator.onePoint==false)
+		{
+			myPrefabReplicator.finalPoint = EditorGUILayout.Vector3Field("Ponto Final",myPrefabReplicator.finalPoint);
+		}
+		myPrefabReplicator.dist = EditorGUILayout.FloatField("Distancia entre objetos",myPrefabReplicator.dist);
+		myPrefabReplicator.prefabby =  (GameObject)EditorGUILayout.ObjectField(myPrefabReplicator.prefabby, typeof(GameObject),false);
+
+		if(GUILayout.Button("Cria os objetos"))
+		{
+			myPrefabReplicator.CopyPrefab();
+		}
+
+	}
+
+}
