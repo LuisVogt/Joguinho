a600a1b567b37318869b60ef5f4437a060ff3691
Luis Vogt <luishenriquevogt@gmail.com>
2017-02-15 13:01:27 -0200

    Elevador Vai e Vem implementado

---
+++ Joguinho/Assets/Scripts/ElevadorVaiEVem.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevadorVaiEVem : MonoBehaviour {
+    public float speed;
+    public float restTime;
+    public GameObject pontoA;
+    public GameObject pontoB;
+    public GameObject Elevador;
+    [SerializeField]private float timePassed;
+    enum estado
+    {
+        goingToA,
+        goingToB,
+        waitingInA,
+        waitingInB
+    }
+
+    [SerializeField]private estado estadoAtual;
+
+	// Use this for initialization
+	void Start () {
+        estadoAtual = estado.waitingInA;
+	}
+
+	void FixedUpdate () {
+        switch (estadoAtual)
+        {
+            case estado.waitingInA:
+                if (timePassed <= 0)
+                    estadoAtual = estado.goingToB;
+                else
+                    timePassed = timePassed - Time.fixedDeltaTime;
+                break;
+            case estado.waitingInB:
+                if (timePassed <= 0)
+                    estadoAtual = estado.goingToA;
+                else
+                    timePassed = timePassed - Time.fixedDeltaTime;
+                break;
+            case estado.goingToA:
+                Elevador.transform.position = Vector3.MoveTowards(Elevador.transform.position, pontoA.transform.position, speed * Time.fixedDeltaTime);
+                if (Elevador.transform.position == pontoA.transform.position)
+                {
+                    timePassed = restTime;
+                    estadoAtual = estado.waitingInA;
+                }
+                break;
+            case estado.goingToB:
+                Elevador.transform.position = Vector3.MoveTowards(Elevador.transform.position, pontoB.transform.position, speed * Time.fixedDeltaTime);
+                if (Elevador.transform.position == pontoB.transform.position)
+                {
+                    timePassed = restTime;
+                    estadoAtual = estado.waitingInB;
+                }
+                break;
+        }
+	}
+}
