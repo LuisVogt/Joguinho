e6e8390e5b4e128421c63b79d611ee636d4ed213
Luis Vogt <luishenriquevogt@gmail.com>
2017-03-29 15:11:57 -0300

    Save/load implementado

---
+++ Joguinho/Assets/Scripts/SaveLoadManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SaveLoadManager : MonoBehaviour {
+    public GameObject PlayerPrefab;
+	public void SaveGame()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Save_Load.Save(player.GetComponent<ColectibleInventory>(), player.GetComponent<StageChecker>());
+    }
+
+    void Start()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    public void LoadGame()
+    {
+        Data loadData = Save_Load.Load();
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if(player == null)
+        {
+            player = Instantiate(PlayerPrefab);
+        }
+        ColectibleInventory CI = player.GetComponent<ColectibleInventory>();
+        CI.clearCollectibles();
+        CI.addCollectibles(loadData.collectibles);
+        DontDestroyOnLoad(player);
+        SceneManager.LoadScene(loadData.level);
+    }
+}
---
+++ Joguinho/Assets/Scripts/Save_Load.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public static class Save_Load{
+    public static void Save(ColectibleInventory CI, StageChecker SC)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream stream = new FileStream(Application.persistentDataPath + "/slot1.sav", FileMode.Create);
+        Data saveData = new Data(CI, SC);
+
+        bf.Serialize(stream, saveData);
+        stream.Close();
+    }
+
+    public static Data Load()
+    {
+        if(File.Exists(Application.persistentDataPath + "/slot1.sav"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream stream = new FileStream(Application.persistentDataPath + "/slot1.sav", FileMode.Open);
+
+            Data loadData = bf.Deserialize(stream) as Data;
+
+            stream.Close();
+            return loadData;
+        }
+        else
+        {
+            Debug.LogError("Erro, save não existe");
+            return new Data();
+        }
+    }
+}
+
+[Serializable]
+public class Data
+{
+    public string level;
+    public List<int> collectibles;
+
+    public Data(ColectibleInventory CI, StageChecker SC)
+    {
+        level = SC.GetCurrentLevel();
+        collectibles = CI.getCollectiblesInInventory();
+        //List<int> tempCollectibleList = CI.getCollectiblesInInventory();
+        /*for (int i = 0; i < tempCollectibleList.Count; i++)
+        {
+            collectibles[i] = tempCollectibleList[i];
+        }*/
+    }
+
+    public Data()
+    {
+        level = "Error";
+        collectibles = new List<int>();
+    }
+}
